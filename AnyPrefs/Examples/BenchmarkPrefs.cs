e5465a3526cc12de45d36243685a792f29e62044
Tzaxast <87723625+Tzaxast@users.noreply.github.com>
2024-01-02 22:01:15 +0200

    Update BenchmarkPrefs.cs

--- AnyPrefs/Examples/BenchmarkPrefs.cs
+++ AnyPrefs/Examples/BenchmarkPrefs.cs
@@ -1,5 +1,5 @@
 ﻿/*
-    Copyright (c) 2021 SecureSoftworks. All rights reserved.
+    Copyright (c) 2023 Tzaxast. All rights reserved.
     Use of this source code is governed by a BSD-style
     license that can be found in the LICENSE file.
 */
